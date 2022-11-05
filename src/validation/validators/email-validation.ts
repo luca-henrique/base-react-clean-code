07f38f4900252097aecedaa5341c8bb17576576c
Lucas Henrique <lukas.paes18@gmail.com>
2022-11-04 21:02:24 -0300

    feature: update projet

--- src/validation/validators/email-validation.ts
+++ src/validation/validators/email-validation.ts
@@ -1,11 +1,14 @@
-import { FieldValidation } from '@/validation/protocols'
-import { InvalidFieldError } from '@/validation/errors'
+import {FieldValidation} from '@/validation/protocols';
+import {InvalidFieldError} from '@/validation/errors';
 
 export class EmailValidation implements FieldValidation {
-  constructor (readonly field: string) {}
+  constructor(readonly field: string) {}
 
-  validate (input: object): Error {
-    const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-    return (!input[this.field] || emailRegex.test(input[this.field])) ? null : new InvalidFieldError()
+  validate(input: object): Error {
+    const emailRegex =
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    return !input[this.field] || emailRegex.test(input[this.field])
+      ? null
+      : new InvalidFieldError();
   }
 }
--- tests/infra/cache/local-storage-adapter.spec.ts
+++ tests/infra/cache/local-storage-adapter.spec.ts
@@ -1,43 +1,34 @@
-import { LocalStorageAdapter } from '@/infra/cache'
+import {LocalStorageAdapter} from '@/infra/cache';
 
-import 'jest-localstorage-mock'
-import faker from 'faker'
+import 'jest-localstorage-mock';
+import faker from 'faker';
 
-const makeSut = (): LocalStorageAdapter => new LocalStorageAdapter()
+const makeSut = (): LocalStorageAdapter => new LocalStorageAdapter();
 
 describe('LocalStorageAdapter', () => {
   beforeEach(() => {
-    localStorage.clear()
-  })
+    localStorage.clear();
+  });
 
   test('Should call localStorage.setItem with correct values', async () => {
-    const sut = makeSut()
-    const key = faker.database.column()
-    const value = faker.random.objectElement<{}>()
+    const sut = makeSut();
+    const key = faker.database.column();
+    const value = faker.random.objectElement<{}>();
 
-    sut.set(key, value)
+    sut.set(key, value);
 
-    expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value))
-  })
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      key,
+      JSON.stringify(value),
+    );
+  });
 
   test('Should call localStorage.removeItem if value is null', async () => {
-    const sut = makeSut()
-    const key = faker.database.column()
+    const sut = makeSut();
+    const key = faker.database.column();
 
-    sut.set(key, undefined)
+    sut.set(key, undefined);
 
-    expect(localStorage.removeItem).toHaveBeenCalledWith(key)
-  })
-
-  test('Should call localStorage.getItem with correct value', async () => {
-    const sut = makeSut()
-    const key = faker.database.column()
-    const value = faker.random.objectElement<{}>()
-    const getItemSpy = jest.spyOn(localStorage, 'getItem').mockReturnValueOnce(JSON.stringify(value))
-
-    const obj = sut.get(key)
-
-    expect(obj).toEqual(value)
-    expect(getItemSpy).toHaveBeenCalledWith(key)
-  })
-})
+    expect(localStorage.removeItem).toHaveBeenCalledWith(key);
+  });
+});
